609a48601d667978ebdbcfcb5abb36ba27759345
Pedro Barbas <152535675+BarbasPedro@users.noreply.github.com>
2024-02-02 12:06:37 -0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+function comprimeJS () {
+  return gulp.src('./source/scripts/*.js')
+  .pipe(uglify())
+  .gulp.dest('./build/scripts')
+}
+
+function comprimeImg () {
+  return gulp.src('./source/images/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./build/images'))
+}
+
+function compilaSass () {
+  return gulp.src('./source/styles/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/styles'))
+}
+
+exports.sass = compilaSass;
+exports.watch = function () {
+  gulp.watch('./source/styles/*.scss', {ignoreInitial:false}, gulp.series(compilaSass));
+  gulp.watch('./source/images/*', {ignoreInitial:false}, gulp.series(comprimeImg));
+  gulp.watch('./source/scripts/*.js', {ignoreInitial:false}, gulp.series(comprimeJS));
+}
