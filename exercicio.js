942703a2528b036e19e5fd3fabf39c016269996c
Pedro Barbas <152535675+BarbasPedro@users.noreply.github.com>
2024-03-07 10:07:36 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const Multiplicacao = (numero1, numero2) => numero1 * numero2;
+console.log(Multiplicacao(5, 10));
+const DizOla = (nome) => console.log(`Olá ${nome}`);
+console.log(DizOla('Pedro'));
---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const Multiplicacao = (numero1: number, numero2: number): number => numero1 * numero2;
+
+console.log(Multiplicacao(5, 10));
+
+const DizOla = (nome: string) => console.log(`Olá ${nome}`);
+
+console.log(DizOla('Pedro'))
